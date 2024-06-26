b78d899cf2b3119e3380c5e9d04a4072692b573d
mehedihasan712277 <mehedihasan712277@gmail.com>
2024-06-27 01:01:37 +0600

    set up done

---
+++ src/asset/data.ts
@@ -0,0 +1,102 @@
+export const movieData = [
+  {
+    id: 1,
+    title: "The Shawshank Redemption",
+    img: "https://i.ibb.co/9yRLZ8N/thmb0.jpg",
+    rating: 9.3,
+    isTrending: true,
+    isBookmarked: false,
+    category: "movie",
+    year: 2015
+  },
+  {
+    id: 2,
+    title: "The Godfather",
+    img: "https://i.ibb.co/SPrdgpN/thmb9.jpg",
+    rating: 9.2,
+    isTrending: false,
+    isBookmarked: false,
+    category: "movie",
+    year: 2016
+  },
+  {
+    id: 3,
+    title: "The Dark Knight",
+    img: "https://i.ibb.co/x1V3v84/thmb8.jpg",
+    rating: 9.0,
+    isTrending: true,
+    isBookmarked: false,
+    category: "movie",
+    year: 2017
+  },
+  {
+    id: 4,
+    title: "The Lord of the Rings: The Return of the King",
+    img: "https://i.ibb.co/XyLQXkW/thmb7.jpg",
+    rating: 8.9,
+    isTrending: false,
+    isBookmarked: false,
+    category: "movie",
+    year: 2018
+  },
+  {
+    id: 5,
+    title: "Pulp Fiction",
+    img: "https://i.ibb.co/kqW0RMr/thmb6.jpg",
+    rating: 8.9,
+    isTrending: true,
+    isBookmarked: false,
+    category: "movie",
+    year: 2019
+  },
+  {
+    id: 6,
+    title: "Breaking Bad",
+    img: "https://i.ibb.co/X4x13RN/thmb5.jpg",
+    rating: 9.5,
+    isTrending: false,
+    isBookmarked: false,
+    category: "series",
+    year: 2020
+  },
+  {
+    id: 7,
+    title: "Game of Thrones",
+    img: "https://i.ibb.co/N1FVb7D/thmb4.jpg",
+    rating: 9.3,
+    isTrending: true,
+    isBookmarked: false,
+    category: "series",
+    year: 2021
+  },
+  {
+    id: 8,
+    title: "Friends",
+    img: "https://i.ibb.co/y4VbWYx/thmb3.jpg",
+    rating: 8.9,
+    isTrending: false,
+    isBookmarked: false,
+    category: "series",
+    year: 2022
+  },
+  {
+    id: 9,
+    title: "The Office (US)",
+    img: "https://i.ibb.co/kqW0RMr/thmb6.jpg",
+    rating: 8.9,
+    isTrending: true,
+    isBookmarked: false,
+    category: "series",
+    year: 2023
+  },
+  {
+    id: 10,
+    title: "Stranger Things",
+    img: "https://i.ibb.co/VMSCDGm/thmb1.jpg",
+    rating: 8.7,
+    isTrending: false,
+    isBookmarked: false,
+    category: "series",
+    year: 2024
+  }
+]
